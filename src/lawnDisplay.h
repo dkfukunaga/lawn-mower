fef43d811d6b5ca60d6a6770a99ec040ca9b0839
dkfukunaga <dkfukunaga@gmail.com>
2024-09-11 20:53:01 -0700

    declared LawnDisplay functions and member variables

--- src/lawnDisplay.h
+++ src/lawnDisplay.h
@@ -3,10 +3,33 @@
 #define LAWN_DISPLAY_H
 
 #include "common.h"
+#include "lawn.h"
+#include "mower.h"
+#include <string>
 
 class LawnDisplay {
 public:
+    LawnDisplay();
+    LawnDisplay(Mower mower);
+    LawnDisplay(Mower mower, Lawn _lawn);
+
+    void        fullRefresh();
+    bool        refreshSquare(Position position);
 private:
+    Mower       _mower;
+    Lawn*       _lawn;
+    Position    _cursor_position;
+
+    bool        drawSquare(Position position);
+    bool        drawMower();
+
+    std::string squareString(Square square);
+
+    void        hideCursor();
+    void        showCursor();
+    void        moveCursor(Position position);
+    void        saveCursorPosition();
+    void        restoreCursorPosition();
 };
 
 #endif
