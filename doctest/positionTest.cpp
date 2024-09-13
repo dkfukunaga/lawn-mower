a216cb403094c137f367c43da89b8e8388d3e2ee
dkfukunaga <dkfukunaga@gmail.com>
2024-09-13 03:32:37 -0700

    created operator tests. all tests passed. TODO updated.

--- doctest/positionTest.cpp
+++ doctest/positionTest.cpp
@@ -85,5 +85,23 @@ TEST_CASE("Test move functions") {
 }
 
 TEST_CASE("Test operator overloads") {
+    Position pos1(4, 7);
+    Position pos2(4, 7);
+    Position pos3(7, 4);
 
+    // verify starting positions
+    REQUIRE(pos1.getX() == 4);
+    REQUIRE(pos1.getY() == 7);
+    REQUIRE(pos2.getX() == 4);
+    REQUIRE(pos2.getY() == 7);
+    REQUIRE(pos3.getX() == 7);
+    REQUIRE(pos3.getY() == 4);
+
+    bool pos1_pos2 = pos1 == pos2;  // true
+    bool pos1_pos3 = pos1 == pos3;  // false
+    bool pos2_pos3 = pos2 != pos3;  // true
+
+    CHECK(pos1_pos2);
+    CHECK_FALSE(pos1_pos3);
+    CHECK(pos2_pos3);
 }
