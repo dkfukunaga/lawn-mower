c02f2fe7ee9c86a7a742e44884e4111816a17059
dkfukunaga <dkfukunaga@gmail.com>
2024-09-13 00:37:30 -0700

    set and move return new Position. added comments.

--- src/position.cpp
+++ src/position.cpp
@@ -4,39 +4,51 @@
 
 /***** CONSTRUCTORS *****/
 
+// Constructor for Position
+// defaults coordinates to (0, 0)
 Position::Position(int x = 0, int y = 0):
     _x(x),
     _y(y) { };
 
+// Copy constructor
 Position::Position(const Position &that):
     _x(that.getX()),
     _y(that.getY()) { };
 
 /***** GETTERS/ACCESSORS *****/
 
+// returns x coordinate
 int Position::getX() const { return _x; }
 
+// returns y coordinate
 int Position::getY() const { return _y; }
 
 /***** SETTERS/MUTATORS *****/
 
-void Position::set(int x, int y) {
+// sets x and y coordinates to new values
+// returns new position
+Position Position::set(int x, int y) {
     _x = x;
     _y = y;
+    return *this;
 }
 
-void Position::move(int x, int y) {
+// moves x and y coordinates by offset
+// returns new position
+Position Position::move(int x, int y) {
     _x += x;
     _y += y;
+    return *this;
 }
 
 /***** OPERATOR OVERRIDES *****/
 
-
+// returns true if x and y are the same, false otherwise
 bool Position::operator==(Position &rhs) {
     return this->_x == rhs._x && this->_y == rhs._y;
 }
 
+// returns true if x or y are different, false otherwise
 bool Position::operator!=(Position &rhs) {
     return !(*this == rhs);
 }
--- src/position.h
+++ src/position.h
@@ -8,17 +8,17 @@ public:
     Position(int x, int y);
     Position(const Position &that);
 
-    int     getX() const;
-    int     getY() const;
+    int         getX() const;
+    int         getY() const;
 
-    void    set(int x, int y);
-    void    move(int x, int y);
+    Position    set(int x, int y);
+    Position    move(int x, int y);
 
-    bool    operator==(Position &rhs);
-    bool    operator!=(Position &rhs);
+    bool        operator==(Position &rhs);
+    bool        operator!=(Position &rhs);
 private:
-    int     _x;
-    int     _y;
+    int         _x;
+    int         _y;
 };
 
 #endif
