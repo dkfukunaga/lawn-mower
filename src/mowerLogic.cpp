4fce7b6792c1cbe7d8144d7466788e96540135ed
dkfukunaga <dkfukunaga@gmail.com>
2024-09-14 19:41:02 -0700

    created mowerLogic class

---
+++ src/mowerLogic.cpp
@@ -0,0 +1,54 @@
+
+#include "mowerLogic.h"
+
+
+
+
+
+
+
+
+
+
+
+// return number of actions taken
+int MowerLogic::orient(Direction direction) {
+
+}
+
+// return SquareType in front of mower
+SquareType MowerLogic::peek() {
+
+}
+
+// forward once
+// return true if successfully moved,
+// false if wall collision
+bool MowerLogic::forward() {
+
+}
+
+// forward n steps
+// if wall collision does not attempt more forward steps
+// return 0 for successful completion,
+// return negative int for difference between steps taken and steps attempted
+int MowerLogic::forward(int steps) {
+
+}
+
+// forward until wall collision
+// return number of steps taken, including last unsuccessful step
+int MowerLogic::forwardToWall() {
+
+}
+
+// turns mower right
+void MowerLogic::turnRight() {
+
+}
+
+// turns mower left
+void MowerLogic::turnLeft() {
+
+}
+
---
+++ src/mowerLogic.h
@@ -0,0 +1,34 @@
+
+#ifndef MOWER_LOGIC_H
+#define MOWEr_LOGIC_H
+
+#include "lawnDisplay.h"
+#include <string>
+
+
+enum class Algorithm {
+    zigzag,
+    spiral,
+};
+
+class MowerLogic {
+public:
+    MowerLogic();
+    MowerLogic(Algorithm algorithm);
+
+    Algorithm       getAlgorithm();
+    bool            setAlogrithm(Algorithm algorithm);
+private:
+    Algorithm       _current_algorithm;
+
+    int             orient(Direction direction);
+    SquareType      peek();
+    bool            forward();
+    int             forward(int steps);
+    int             forwardToWall();
+    void            turnRight();
+    void            turnLeft();
+};
+
+
+#endif
