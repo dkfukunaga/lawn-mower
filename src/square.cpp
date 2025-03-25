e8405b2f1b4860b35484cec706551ee82ecd7c9b
dkfukunaga <dkfukunaga@gmail.com>
2025-03-24 18:44:43 -0700

    created Square class, SquareType enum, and Coordinates struct

---
+++ src/square.cpp
@@ -0,0 +1,38 @@
+
+#include "square.h"
+#include <string>
+#include <random>
+
+void            Square::setType(SquareType type) { type_ = type; }
+void            Square::setCoordinates(int x, int y) { coordinates_ = Coordinates{x, y}; }
+void            Square::setCoordinates(Coordinates coordinates) {  coordinates_ = coordinates; }
+
+SquareType      Square::getType() const { return type_; }
+Coordinates     Square::getCoordinates() const { return coordinates_; }
+
+
+std::string     Square::getString() const {
+    std::string str;
+
+    switch (type_) {
+    case SquareType::unmowed:
+        switch (rand() % 2) {
+        case 0:
+            str = "\",";
+        default:
+            str = ",\"";
+        }
+        break;
+    case SquareType::mowed:
+        str = " .";
+        break;
+    case SquareType::wall:
+        str = "  ";
+        break;
+    default:
+        str = "??";
+        break;
+    }
+
+    return str;
+}
---
+++ src/square.h
@@ -0,0 +1,37 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+#endif
+
+#include <string>
+
+enum class SquareType {
+    unmowed,
+    mowed,
+    wall,
+};
+
+struct Coordinates {
+    int             x;
+    int             y;
+};
+
+class Square {
+public:
+    Square(SquareType type, int x, int y):
+        type_(type),
+        coordinates_(Coordinates{x, y}) { };
+    Square():
+        type_(SquareType::unmowed),
+        coordinates_(Coordinates{-1,-1}) { };
+    
+    void            setType(SquareType type);
+    void            setCoordinates(int x, int y);
+    void            setCoordinates(Coordinates coordinates);
+
+    SquareType      getType() const;
+    Coordinates     getCoordinates() const;
+    std::string     getString() const;
+private:
+    SquareType      type_;
+    Coordinates     coordinates_;
+};
