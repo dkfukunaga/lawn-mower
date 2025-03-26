565c2e4083fa4ebb5d6291416037268b85d0a8e1
dkfukunaga <dkfukunaga@gmail.com>
2025-03-26 01:17:41 -0700

    removed superfluous static_cast for facing

--- src/mower.cpp
+++ src/mower.cpp
@@ -67,7 +67,7 @@ int                 Mower::getSteps() const { return steps_; }
 
 int                 Mower::getTurns() const { return turns_; }
 
-std::string         Mower::getMowerString() const { return MOWER_STRINGS[static_cast<int>(facing_)]; }
+std::string         Mower::getMowerString() const { return MOWER_STRINGS[facing_]; }
 
 std::string         Mower::getFacingString() const { return Facing::toString(facing_); }
 
