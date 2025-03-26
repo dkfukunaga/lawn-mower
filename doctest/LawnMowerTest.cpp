b6e11ffdfb39e6c36e92826edb4f8fdd27740b5b
dkfukunaga <dkfukunaga@gmail.com>
2025-03-26 15:58:00 -0700

    created unit tests for Lawn (setLawn/Width/Height and getString not tested)

--- doctest/LawnMowerTest.cpp
+++ doctest/LawnMowerTest.cpp
@@ -1,9 +1,16 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "..\src\square.h"
+#include "..\src\coordinates.h"
+#include "..\src\facing.h"
+#include "..\src\lawn.h"
 #include <string>
+#include <random>
+
+
 
 TEST_CASE("Square Test") {
+    srand(0);
     Coordinates testCoordinates{1,4};
 
     Square *defaultSquare = new Square();
@@ -43,11 +50,74 @@ TEST_CASE("Square Test") {
     }
 
     SUBCASE("Square setter tests") {
-        
+        defaultSquare->setType(SquareType::mowed);
+        CHECK(defaultSquare->getType() == SquareType::mowed);
+        defaultSquare->setType(SquareType::wall);
+        CHECK(defaultSquare->getType() == SquareType::wall);
+        defaultSquare->setType(SquareType::unmowed);
+        CHECK(defaultSquare->getType() == SquareType::unmowed);
+        defaultSquare->setType(SquareType::error);
+        CHECK(defaultSquare->getType() == SquareType::error);
     }
 
     delete defaultSquare;
     delete mowedSquare;
     delete unmowedSquare;
     delete wallSquare;
+}
+
+TEST_CASE("Lawn Default Constructor Tests") {
+    srand(0);
+    Lawn* test_lawn = new Lawn();
+
+    SUBCASE("Test Lawn initialization") {
+        // top wall
+        CHECK(test_lawn->getSquare(Coordinates{0,0}).getType() == SquareType::wall);
+        CHECK(test_lawn->getSquare(Coordinates{5,0}).getType() == SquareType::wall);
+        CHECK(test_lawn->getSquare(Coordinates{9,0}).getType() == SquareType::wall);
+        
+        // left wall
+        CHECK(test_lawn->getSquare(Coordinates{0,1}).getType() == SquareType::wall);
+        CHECK(test_lawn->getSquare(Coordinates{0,5}).getType() == SquareType::wall);
+        CHECK(test_lawn->getSquare(Coordinates{0,8}).getType() == SquareType::wall);
+        
+        // right wall
+        CHECK(test_lawn->getSquare(Coordinates{9,1}).getType() == SquareType::wall);
+        CHECK(test_lawn->getSquare(Coordinates{9,5}).getType() == SquareType::wall);
+        CHECK(test_lawn->getSquare(Coordinates{9,8}).getType() == SquareType::wall);
+
+        // unmowed
+        CHECK(test_lawn->getSquare(Coordinates{1,1}).getType() == SquareType::unmowed);
+        CHECK(test_lawn->getSquare(Coordinates{8,1}).getType() == SquareType::unmowed);
+        CHECK(test_lawn->getSquare(Coordinates{5,5}).getType() == SquareType::unmowed);
+        CHECK(test_lawn->getSquare(Coordinates{1,8}).getType() == SquareType::unmowed);
+        CHECK(test_lawn->getSquare(Coordinates{8,8}).getType() == SquareType::unmowed);
+    }
+
+    SUBCASE("Test mowSquare()") {
+        test_lawn->mowSquare(Coordinates{1,1});
+        test_lawn->mowSquare(Coordinates{2,3});
+        test_lawn->mowSquare(Coordinates{4,1});
+        test_lawn->mowSquare(Coordinates{7,8});
+        test_lawn->mowSquare(Coordinates{8,8});
+        // these should not mow the square
+        test_lawn->mowSquare(Coordinates{0,0});
+        test_lawn->mowSquare(Coordinates{0,9});
+        test_lawn->mowSquare(Coordinates{9,0});
+        test_lawn->mowSquare(Coordinates{9,9});
+
+        CHECK(test_lawn->getSquare(Coordinates{1,1}).getType() == SquareType::mowed);
+        CHECK(test_lawn->getSquare(Coordinates{2,3}).getType() == SquareType::mowed);
+        CHECK(test_lawn->getSquare(Coordinates{4,1}).getType() == SquareType::mowed);
+        CHECK(test_lawn->getSquare(Coordinates{7,8}).getType() == SquareType::mowed);
+        CHECK(test_lawn->getSquare(Coordinates{8,8}).getType() == SquareType::mowed);
+        
+        CHECK_FALSE(test_lawn->getSquare(Coordinates{0,0}).getType() == SquareType::mowed);
+        CHECK_FALSE(test_lawn->getSquare(Coordinates{0,9}).getType() == SquareType::mowed);
+        CHECK_FALSE(test_lawn->getSquare(Coordinates{9,0}).getType() == SquareType::mowed);
+        CHECK_FALSE(test_lawn->getSquare(Coordinates{9,9}).getType() == SquareType::mowed);
+    }
+
+
+    delete test_lawn;
 }
